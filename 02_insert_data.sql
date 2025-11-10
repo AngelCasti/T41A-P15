06aeb3866722c0733e42832e9cba5da328a685d7
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-09 22:52:14 -0600

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO productos (nombre, precio, stock) VALUES
+('Laptop', 999.99, 10),
+('Pantalla', 300.50, 5),
+('Memoria', 85.00, 7),
+('Ratón', 99.90, 8),
+('Ventilador', 348.00, 9),
+('Silla', 215.75, 4),
+('SSD', 59.99, 3),
+('Celular', 599.00, 2),
+('Cargador', 39.99, 1),
+('Cable', 45.20, 6);
+
+INSERT INTO departamentos (nombre_depto) VALUES
+('Ventas'),
+('Ingeniería'),
+('Desarrollo');
+
+INSERT INTO empleados (nombre, email, id_depto) VALUES
+('Luis David', 'luis@ejemplo.com', 1),
+('Angel Castillo', 'angel@ejemplo.com', 2),
+('Rafael Sánchez', 'rafael@ejemplo.com', 1),
+('Cruz Angel', 'cruz@ejemplo.com', 2),
+('Yazmín Guerrero', 'yazmin@ejemplo.com', 3);
